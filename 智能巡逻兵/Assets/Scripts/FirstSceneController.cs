e609d609c97258d42d9db2d262cdf8cdbb4fee9f
lurui7 <3287377676@qq.com>
2020-11-18 18:40:55 +0800

    hw7

---
+++ 智能巡逻兵/Assets/Scripts/FirstSceneController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FirstSceneController : MonoBehaviour, IUserAction, ISceneController
+{
+    public PropFactory patrol_factory;                               //巡逻者工厂
+    public ScoreRecorder recorder;                                   //记录员
+    public PatrolActionManager action_manager;                       //运动管理器
+    public int wall_sign = -1;                                       //当前玩家所处哪个格子
+    public GameObject player;                                        //玩家
+    public Camera main_camera;                                       //主相机
+    public float player_speed = 5;                                   //玩家移动速度
+    public float rotate_speed = 135f;                                //玩家旋转速度
+    private List<GameObject> patrols;                                //场景中巡逻者列表
+                              //场景水晶列表
+    private bool game_over = false;                                  //游戏结束
+    
+    public class Diretion {
+        public const int UP = 0;
+        public const int DOWN = 2;
+        public const int LEFT = -1;
+        public const int RIGHT =1;
+    }
+    void Update()
+    {
+        for (int i = 0; i < patrols.Count; i++)
+        {
+            patrols[i].gameObject.GetComponent<PatrolData>().wall_sign = wall_sign;
+        }
+        
+    }
+    void Start()
+    {
+        SSDirector director = SSDirector.GetInstance();
+        director.CurrentScenceController = this;
+        patrol_factory = Singleton<PropFactory>.Instance;
+        action_manager = gameObject.AddComponent<PatrolActionManager>() as PatrolActionManager;
+        LoadResources();
+        main_camera.GetComponent<CameraFlow>().follow = player;
+        recorder = Singleton<ScoreRecorder>.Instance;
+    }
+
+    public void LoadResources()
+    {
+        Instantiate(Resources.Load<GameObject>("Prefabs/Plane"));
+        player = Instantiate(Resources.Load("Prefabs/Player"), new Vector3(0, 9, 0), Quaternion.identity) as GameObject;
+        
+        patrols = patrol_factory.GetPatrols();
+        //所有侦察兵移动
+        for (int i = 0; i < patrols.Count; i++)
+        {
+            action_manager.GoPatrol(patrols[i]);
+        }
+    }
+    //玩家移动
+    public void MovePlayer(int dir)
+    {
+        if(!game_over)
+        {
+            if (dir == 0 || dir == 1 || dir == -1 || dir == 2)
+            {
+                player.GetComponent<Animator>().SetBool("run", true);
+            }
+            else
+            {
+                player.GetComponent<Animator>().SetBool("run", false);
+            }
+            //移动和旋转
+            //player.transform.Translate(0, 0, translationZ * player_speed * Time.deltaTime);
+            //player.transform.Rotate(0, translationX * rotate_speed * Time.deltaTime, 0);
+            player.transform.rotation = Quaternion.Euler(new Vector3(0, dir * 90, 0));
+        switch (dir) {
+            case Diretion.UP:
+                player.transform.position += new Vector3(0, 0, 0.1f);
+                break;
+            case Diretion.DOWN:
+                player.transform.position += new Vector3(0, 0, -0.1f);
+                break;
+            case Diretion.LEFT:
+                player.transform.position += new Vector3(-0.1f, 0, 0);
+                break;
+            case Diretion.RIGHT:
+                player.transform.position += new Vector3(0.1f, 0, 0);
+                break;
+        }
+            //防止碰撞带来的移动
+            if (player.transform.localEulerAngles.x != 0 || player.transform.localEulerAngles.z != 0)
+            {
+                player.transform.localEulerAngles = new Vector3(0, player.transform.localEulerAngles.y, 0);
+            }
+            if (player.transform.position.y != 0)
+            {
+                player.transform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+            }     
+        }
+    }
+
+    public int GetScore()
+    {
+        return recorder.GetScore();
+    }
+
+    
+    public bool GetGameover()
+    {
+        return game_over;
+    }
+    public void Restart()
+    {
+        //SceneManager.LoadScene("Scenes/mySence");
+        SceneManager.LoadScene("sample");
+    }
+
+    void OnEnable()
+    {
+        GameEventManager.ScoreChange += AddScore;
+        GameEventManager.GameoverChange += Gameover;
+       
+    }
+    void OnDisable()
+    {
+        GameEventManager.ScoreChange -= AddScore;
+        GameEventManager.GameoverChange -= Gameover;
+     
+    }
+    
+    void AddScore()
+    {
+        recorder.AddScore();
+    }
+    void Gameover()
+    {
+        game_over = true;
+        patrol_factory.StopPatrol();
+        action_manager.DestroyAllAction();
+    }
+}
---
+++ 智能巡逻兵/Assets/Scripts/UserGUI.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour {
+
+    private IUserAction action;
+    private GUIStyle score_style = new GUIStyle();
+    private GUIStyle text_style = new GUIStyle();
+    private GUIStyle over_style = new GUIStyle();
+    
+    //private bool game_start = false;
+    //控制规则显示
+    bool isShow = false;
+    
+    void Start ()
+    {
+        action = SSDirector.GetInstance().CurrentScenceController as IUserAction;
+        text_style.normal.textColor = new Color(0, 0, 0, 1);
+        text_style.fontSize = 16;
+        score_style.normal.textColor = new Color(1,0,0,1);
+        score_style.fontSize = 16;
+        over_style.fontSize = 25;
+    }
+
+    public class Diretion {
+        public const int UP = 0;
+        public const int DOWN = 2;
+        public const int LEFT = -1;
+        public const int RIGHT = 1;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            action.MovePlayer(Diretion.UP);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            action.MovePlayer(Diretion.DOWN);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            action.MovePlayer(Diretion.LEFT);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            action.MovePlayer(Diretion.RIGHT);
+        }
+    }
+    
+    private void OnGUI()
+    {   
+        GUIStyle button_style = new GUIStyle("button")
+		{
+			fontSize = 15
+		};
+        
+
+        if (GUI.Button(new Rect(10, 10, 60, 30), "Rule", button_style))
+		{
+			if (isShow)
+				isShow = false;
+			else
+				isShow = true;
+		}
+		if(isShow)
+		{
+			GUI.Label(new Rect(Screen.width / 2 - 400 , 70, 100, 50), "按方向键移动", text_style);
+            GUI.Label(new Rect(Screen.width / 2 - 400 , 90, 150, 50), "成功躲避巡逻兵追捕加1分", text_style);
+		}
+
+        //if (game_start) {
+            GUI.Label(new Rect(Screen.width - 150, 5, 200, 50), "分数:", text_style);
+            GUI.Label(new Rect(Screen.width - 100, 5, 200, 50), action.GetScore().ToString(), score_style);
+
+            if(action.GetGameover())
+            {
+                GUI.Label(new Rect(Screen.width / 2 - 50, Screen.width / 2 - 250, 100, 100), "游戏结束", over_style);
+                if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.width / 2 - 200, 100, 50), "重新开始"))
+                {
+                    action.Restart();
+                    return;
+                }
+            }
+        //}
+        // else {
+        //     GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 100, 100, 100), "智能巡逻兵", over_style);
+            
+        //     if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2, 100, 50), "开始游戏")) {
+        //         game_start = true;
+        //         action.Restart();
+        //     }
+        // }
+        
+        
+    }
+}
